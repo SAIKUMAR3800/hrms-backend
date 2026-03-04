a9173d45e0fa005cc85eb834fe22c0352cf6e7c5
GILLEDI SAI KUMAR <saigilledi@gmail.com>
2026-03-04 11:50:43 +0530

    updated schemas

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Attendance(Base):
     __tablename__ = "attendance"
 
     id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employees.id"))
+    employee_id = Column(String, ForeignKey("employees.id"))
     date = Column(Date, nullable=False)
     status = Column(String, nullable=False)
 
--- schemas.py
+++ schemas.py
@@ -20,14 +20,14 @@ class EmployeeResponse(BaseModel):
 
 
 class AttendanceCreate(BaseModel):
-    employee_id: int
+    employee_id: str
     date: date
     status: str
 
 
 class AttendanceResponse(BaseModel):
     id: int
-    employee_id: int
+    employee_id: str
     date: date
     status: str
 
